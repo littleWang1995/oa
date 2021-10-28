b3266306efde4118788adc597a97b9cc47b59320
wj <wj@163.com>
2021-10-28 20:40:48 +0800

    调用sayHello方法

--- oa-organ/src/main/java/com/wj/oa/organ/service/impl/TestServiceImpl.java
+++ oa-organ/src/main/java/com/wj/oa/organ/service/impl/TestServiceImpl.java
@@ -2,11 +2,11 @@ package com.wj.oa.organ.service.impl;
 
 public class TestServiceImpl {
 	public static void main(String[] args) {
-		
+		sayHello();
 	}
 
 	
-	private void sayHello() {
+	private static void sayHello() {
 		System.out.println("hello 1");
 	}
 }
