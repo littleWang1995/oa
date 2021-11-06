b3e23090ec6dc6e45dc2e1ae429e5f4072e0b158
wj <wj@163.com>
2021-11-06 12:40:09 +0800

    张三修改代码
    完成了部门管理的开发任务
    
    这次不知道提交了什么sss

---
+++ oa-organ/src/main/java/com/wj/oa/organ/service/impl/DepartmentImpl.java
@@ -0,0 +1,9 @@
+package com.wj.oa.organ.service.impl;
+
+public class DepartmentImpl {
+	public static void main(String[] args) {
+		System.out.println("部门管理-部分开发完成");
+		System.out.println("部门管理-80开发完成");
+	}
+
+}
